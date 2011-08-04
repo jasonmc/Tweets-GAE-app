443eee08c5c5aad8fdd455f19bdb962c8a090f25
Jason McCandless <jasonmccandless@gmail.com>
2011-08-04 02:09:27 +0100

    Move libraries to seperate directory.

--- fmain.py
+++ fmain.py
@@ -1,4 +1,8 @@
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 from tweets import app
+
 
 if __name__ == '__main__':
     app.run()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from google.appengine.ext.webapp.util import run_wsgi_app
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 from tweets import app
 
 run_wsgi_app(app)
