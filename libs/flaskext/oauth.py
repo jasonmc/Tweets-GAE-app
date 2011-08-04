e58bdad919fe5778b0db0f0132d89d10c21b08b8
Jason McCandless <jasonmccandless@gmail.com>
2011-08-04 17:03:20 +0100

    Fix proxy_info not set when on other machines.

--- libs/flaskext/oauth.py
+++ libs/flaskext/oauth.py
@@ -203,10 +203,13 @@ class OAuthRemoteApp(object):
         Usually you don't have to do that but use the :meth:`request`
         method instead.
         """
+
         from socket import gethostname
         if gethostname().lower() == 'mclovin':
             import socks
             proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, 'www-proxy.cs.tcd.ie', 8080)
+        else:
+            proxy_info = None
         return oauth2.Client(self._consumer, self.get_request_token(),proxy_info=proxy_info)
 
     def request(self, url, data=None, headers=None, format='urlencoded',
